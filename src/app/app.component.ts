31a0edd14edb76f4f2d58797d15c362d9e802d94
Boris Ermachenko <cbkf12.ru@yandex.ru>
2019-08-04 19:53:34 +0300

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GreetPageComponent } from './greet-page/greet-page.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GreetPageComponent } from './greet-page/greet-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetPageComponent,
+    FirstPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
